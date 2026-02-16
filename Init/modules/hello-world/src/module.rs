ce783b8c5a7e62851f5f2a57999c96f9fb62e4a8
Bechma <19294519+Bechma@users.noreply.github.com>
2026-02-16 17:05:41 +0100

    creation of the init template to fire up a project
    Signed-off-by: Bechma <19294519+Bechma@users.noreply.github.com>

---
+++ Init/modules/hello-world/src/module.rs
@@ -0,0 +1,35 @@
+use modkit::{Module, ModuleCtx, RunnableCapability, async_trait};
+
+#[derive(Default)]
+#[modkit::module(name = "hello-world", capabilities = [stateful])]
+pub struct HelloWorldModule;
+
+#[async_trait]
+impl Module for HelloWorldModule {
+    async fn init(&self, _ctx: &ModuleCtx) -> modkit::Result<()> {
+        tracing::info!("Init hello world module");
+        Ok(())
+    }
+}
+
+#[async_trait]
+impl RunnableCapability for HelloWorldModule {
+    async fn start(&self, cancel: tokio_util::sync::CancellationToken) -> modkit::Result<()> {
+        tokio::spawn(async move {
+            loop {
+                tokio::select! {
+                    _ = cancel.cancelled() => break,
+                    _ = tokio::time::sleep(tokio::time::Duration::from_secs(1)) => {
+                        tracing::info!("Hello World");
+                    }
+                }
+            }
+        });
+        Ok(())
+    }
+
+    async fn stop(&self, _cancel: tokio_util::sync::CancellationToken) -> modkit::Result<()> {
+        tracing::info!("Goodbye World");
+        Ok(())
+    }
+}
